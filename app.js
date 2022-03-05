e11e93d710afab74cf8202831ed461de32687d04
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-05 19:36:03 +0500

    Mongoose connected

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import routes from "./routes/index.js";
-
+import mongoose from "mongoose";
 const main = async () => {
   const app = express();
 
   app.use(express.static("public"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlg2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  );
   app.use("/", routes);
 
   app.listen(3000, () => {
