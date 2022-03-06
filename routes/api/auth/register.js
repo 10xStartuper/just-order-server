d11c515ee52c4289e1f66c360c64494dd20dcfd8
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-06 18:33:23 +0500

    error handling

--- routes/api/auth/register.js
+++ routes/api/auth/register.js
@@ -7,19 +7,25 @@ const router = Router();
 
 router.post("/", async (req, res, next) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = await User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: hashedPassword,
-    phoneNumber: req.body.phoneNumber,
-    dateOfBirth: req.body.dateOfBirth,
-  });
-  user.password = undefined;
-  const accessToken = jwt.sign({ user }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-  res.status(201).json({ accessToken: accessToken });
+  try {
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPassword,
+      phoneNumber: req.body.phoneNumber,
+      dateOfBirth: req.body.dateOfBirth,
+    });
+    console.log("User has been created");
+    user.password = undefined;
+    const accessToken = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    res.status(201).json({ accessToken: accessToken });
+  } catch (e) {
+    console.log("Error creating user");
+    res.status(401).json({ error: e });
+  }
 });
 
 export default router;
