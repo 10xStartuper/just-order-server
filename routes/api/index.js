3624fcc82e817cc022b2f18a2286257170549363
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-08 14:40:40 +0500

    Product CRUD

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import auth from "./auth/index.js";
+import products from "./products/product.js";
 
 const router = Router();
 
 router.use("/auth", auth);
+router.use("/products", products);
 
 export default router;
