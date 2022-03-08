e8624b42a1e2a59d517bb796349da947060561d9
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-08 15:32:35 +0500

    Home page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ import { Router } from "express";
 import api from "./api/index.js";
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.render("page", {
+    title: "Welcome to YesU API",
+    content: "Use {/api/} route to interact with our API",
+  });
+});
+
 router.use("/api/", api);
 
 router.use("*", (req, res) => {
